7ab914cca0a5c39836da3e2debb2879d69b2c2fb
Vlad Klymenko <klymenvlad@gmail.com>
2025-03-04 21:41:11 +0100

    feat: crud reservations, started to implement user module, app is dockerized

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
+import { DatabaseModule, LoggerModule } from '@app/common';
+import { ReservationsRepository } from './reservations.repository';
+import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }]),
+    LoggerModule
+  ],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService, ReservationsRepository],
 })
-export class ReservationsModule {}
+export class ReservationsModule {
+}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,41 +4,40 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import eslintPluginNestJs from 'eslint-plugin-nestjs';
-import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
-    {
-      ignores: ['eslint.config.mjs'],
-    },
-    eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    eslintPluginPrettierRecommended,
-    eslintPluginNestJs.configs.recommended,
-    {
-      languageOptions: {
-        globals: {
-          ...globals.node,
-          ...globals.jest,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        parserOptions: {
-          projectService: true,
-          tsconfigRootDir: import.meta.dirname,
-        },
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  eslintPluginNestJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
       },
     },
-    {
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-floating-promises': 'warn',
-        '@typescript-eslint/no-unsafe-argument': 'warn',
-        '@typescript-eslint/explicit-function-return-type': 'warn',
-        '@typescript-eslint/no-misused-promises': 'error',
-        'nestjs/use-validation-pipe': 'error',
-        'nestjs/use-injectable-providers': 'warn',
-        'simple-import-sort/imports': 'error',
-        'simple-import-sort/exports': 'error',
-      },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-misused-promises': 'error',
+      'nestjs/use-validation-pipe': 'error',
+      'nestjs/use-injectable-providers': 'warn',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
+  },
 );
